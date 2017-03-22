e1a6d021e19ff2d37141ca924efcfd0e556e5e13
Gen <genalyn.estrada14@gmail.com>
2017-03-22 23:42:50 +0000

    Refactored to create factory method design pattern, refactored Event to do everything Cell did, created EventCollection to do everything CellCollection did, deleted Cell and CellCollection. Program still needs to return the cheapest ticket per event.

---
+++ src/main/java/EventCollection.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Genalyn Estrada on 22/03/17.
+ */
+public class EventCollection {
+}
---
+++ src/test/java/EventCollectionTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Genalyn Estrada on 22/03/17.
+ */
+public class EventCollectionTest {
+
+}
